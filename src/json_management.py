9617baab1daf2bf3b2efa53a3617179ac9005fda
Dmitry Krasheninnikov <ice_eyes@mail.ru>
2024-04-13 20:00:31 +0300

    Decorator name load_and_save_file is created for sort_by_salary, get_top and for delete_vacancy methods.

--- src/json_management.py
+++ src/json_management.py
@@ -20,7 +20,7 @@ class ManageJSON(ABC):
         self.dict_list = []
 
     @abstractmethod
-    def save_file(self):
+    def load_and_save_file(self):
         pass
 
     @abstractmethod
@@ -41,49 +41,34 @@ class JSONManagement(ManageJSON):
     def __init__(self, vacancy_list: [HH]) -> None:
         super().__init__(vacancy_list)
 
-    def read_file(func):
+    def load_and_save_file(func):
         """Функция-декоратор читает файл и преобразует список в ЭК Vacancy"""
         def wrapper(self, *args, **kwargs):
             with open(self.file_name, mode='r', encoding='utf-8') as file:
                 self.dict_list = [Vacancy(**_) for _ in json.load(file)]
-                [print(_) for _ in self.dict_list]
-                func()
-                return self.dict_list
-        return wrapper
-
-    def save_file(func):
-        """Функция-декоратор открытия файла на чтение и занесение в файл данных, то есть преобразование
-        ЭК Vacancy в обычный словарь и сохранение в файл"""
-
-        def wrapper(self, *args, **kwargs) -> None:
+            func(self)
             with open(self.file_name, mode='w', encoding='utf-8') as file:
                 list_ = []
-                for vacancy in args:
+                for vacancy in self.dict_list:
                     list_.append(vacancy.get_self_dict)
                 s = json.dumps(list_, ensure_ascii=False)
                 file.write(s)
         return wrapper
 
-    @save_file
-    @read_file
+    @load_and_save_file
     def sort_by_salary(self):
         # Сортируем по убыванию
         self.dict_list.sort(reverse=True, key=lambda x: x.salary)
         # Печатаем сортировку
         print(f'ВЫВОЖУ РЕЗУЛЬТАТЫ СОРТИРОВКИ')
         [print(_) for _ in self.dict_list]
-        return self.dict_list
+
 
     def get_top(self, num):
-        """Печатаем топ 10 вакансий, на вход получает количество вакансий, которые надо отобразить"""
-        with open(self.file_name, mode='r', encoding='utf-8') as file:
-            # Преобразуем список словарей в ЭК Vacancy
-            dict_list = [Vacancy(**_) for _ in json.load(file)]
-            # Сортируем по убыванию
-            dict_list.sort(reverse=True, key=lambda x: x.salary)
-            # Печатаем сортировку
-            print(f'ВЫВОЖУ ТОП-10')
-            [print(_) for _ in dict_list[:10]]
+        """Печатаем топ num вакансий, на вход получает количество вакансий, которые надо отобразить"""
+        # Печатаем сортировку
+        print(f'ВЫВОЖУ ТОП-{num}')
+        [print(_) for _ in self.dict_list[:num]]
     def delete_vacancy(self, index):
         ...
 
